2567d6c9a3fe1e1cd00d0fc76c65fab68cc1e8a6
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-10-03 10:15:41 -0700

    username moved to py_modules

---
+++ username.py
@@ -0,0 +1,17 @@
+import re
+
+# Open the username.txt file for reading
+with open("Username.txt", "r") as f:
+    # Read the entire file contents into a string
+    text = f.read()
+
+# Compile the regular expression for extracting usernames
+username_regex = re.compile(r"@[^\s]+")
+
+# Extract all of the usernames from the text
+usernames = username_regex.findall(text)
+
+# Print the extracted usernames
+for username in usernames:
+    print(username)
+
